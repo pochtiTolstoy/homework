5bd5a70e56db098513566844b9411bc47be8868c
Tim <90455698+pochtiTolstoy@users.noreply.github.com>
2021-12-17 11:05:20 +0300

    Update Get the Middle Character.cpp

--- CodeWars/Get the Middle Character.cpp
+++ CodeWars/Get the Middle Character.cpp
@@ -11,3 +11,14 @@ std::string get_middle(std::string input)
   sum += input[n/2];
   return sum;
 }
+
+// std::string get_middle(std::string input) 
+// {
+//   int i = input.length();
+
+//   if ((i % 2) == 0) 
+//     return input.substr(i/2 - 1, 2);
+    
+//   else
+//     return input.substr(i/2, 1);
+// }
