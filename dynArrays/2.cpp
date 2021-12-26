34363c863f610cbc8b29027addde101daa31ea56
Tim <90455698+pochtiTolstoy@users.noreply.github.com>
2021-12-26 18:39:50 +0300

    Update 2.cpp

--- dynArrays/2.cpp
+++ dynArrays/2.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
 
+//Функцию украл... признаюсь :)
+int getRandomNumber(int min, int max)
+{
+    static const double fraction = 1.0 / (static_cast<double>(RAND_MAX) + 1.0); 
+    return static_cast<int>(rand() * fraction * (max - min + 1) + min);
+}
+
 int main()
 {
     int n, m;
@@ -13,7 +20,7 @@ int main()
     {
         for (int j = 0; j < n; ++j)
         {
-            matrix[i][j] = rand()%100001;
+            matrix[i][j] = getRandomNumber(0, 100000);
         }
     }
 
@@ -22,7 +29,7 @@ int main()
     {
         for (int j = 0; j < n; ++j)
         {
-            std::cout << matrix[i][j] << "\t";
+            std::cout << matrix[i][j] << "\t" << "\t";
             
             // for (int space = )
         }
