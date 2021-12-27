6d3cc25c62733a3986b186b58f01a3beaa6a7897
Tim <90455698+pochtiTolstoy@users.noreply.github.com>
2021-12-28 01:56:43 +0300

    Update 7.cpp

--- dynArrays/7.cpp
+++ dynArrays/7.cpp
@@ -1,28 +1,26 @@
 #include <iostream>
-
-void shiftArray(int A[], int n, int k)
+//reverse method
+void reverse(int arr[], int l, int h)
 {
-    int counter = 0;
-        k %= n;
-    
-    
-    while (k--)
-    {
-        int temp = A[0];
-        for (int i = 0; i < n; ++i)
-        {
-            A[i] = A[i+1];
-            std::cout << A[i+1] << " ";
-        }
-        A[n-1] = temp;
+    for (int low = l, high = h; low < high; low++, high--) {
+        std::swap(arr[low], arr[high]);
     }
 }
 
 int main()
 {
-    int A[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-    shiftArray(A, 10, 1);
-    std::cout << "\n";
+    const int len = 10;
+    int indShift;
+    std::cout << "Input shift value: ";
+    std::cin >> indShift;
+    indShift %= len;
+
+    int A[len] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    reverse(A, 0, len-1);
+    reverse(A, 0, indShift - 1);
+    reverse(A, indShift, len-1);
+
+
 
     for(int i = 0; i < 10; ++i)
     {
@@ -31,3 +29,37 @@ int main()
     std::cout << "\n";
     return 0;
 }
+
+// #include <iostream>
+
+// void shiftArray(int A[], int n, int k)
+// {
+//     int counter = 0;
+//         k %= n;
+    
+    
+//     while (k--)
+//     {
+//         int temp = A[0];
+//         for (int i = 0; i < n; ++i)
+//         {
+//             A[i] = A[i+1];
+//             std::cout << A[i+1] << " ";
+//         }
+//         A[n-1] = temp;
+//     }
+// }
+
+// int main()
+// {
+//     int A[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+//     shiftArray(A, 10, 1);
+//     std::cout << "\n";
+
+//     for(int i = 0; i < 10; ++i)
+//     {
+//         std::cout << A[i];
+//     }
+//     std::cout << "\n";
+//     return 0;
+// }
